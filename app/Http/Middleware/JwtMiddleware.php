9142bc4a458f0be9f8791bb7e952859d3f3c0b23
Yaroslav <ray.last.light@gmail.com>
2021-10-19 17:50:02 +0300

    Create JwtMiddleware.php
    json handlers

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\JWT;
+use JWTAuth;
+
+class JwtMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if($e instanceof TokenInvalidException){
+                return response()->json(['status' => 'Token is Invalid']);
+            }elseif ($e instanceof TokenExpiredException){
+                return response()->json(['status' => 'Token is Expired']);
+            }else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+
+        return $next($request);
+    }
+}
